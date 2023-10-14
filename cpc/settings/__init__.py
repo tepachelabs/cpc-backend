c739a8f5dcafcccc41ee9e3b7b92a92f3adf7457
Jose Salcido <jmsalcidoaguilar@gmail.com>
2023-10-14 10:20:12 -0700

    allow hosts, remove error 400

--- cpc/settings/__init__.py
+++ cpc/settings/__init__.py
@@ -2,6 +2,7 @@ from ._base import *
 from .apps import *
 from .databases import *
 from .django import *
+from .logging import *
 from .middlewares import *
 from .sentry import *
 from .telegram import *
