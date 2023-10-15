9279b8162984111de4780e85178cb4d39dc7820e
jmsalcido <jose@perro.cafe>
2023-10-15 16:32:17 -0700

    use inmemory huey

--- cpc/app/tasks.py
+++ cpc/app/tasks.py
@@ -7,7 +7,8 @@ from cpc.settings import PRODUCTION
 
 logger = logging.getLogger(__name__)
 
-huey = FileHuey() if PRODUCTION else MemoryHuey()
+# huey = FileHuey() if PRODUCTION else MemoryHuey()
+huey = MemoryHuey()
 
 
 @huey.periodic_task(crontab(minute="*"))
