9d5756748531078fd00b650868a08f409d5054cd
Jose Salcido <jmsalcidoaguilar@gmail.com>
2024-05-14 19:07:00 -0700

    send the /privacy route

--- cpc/app/urls.py
+++ cpc/app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index_view, name="index"),
+    path("privacy", views.privacy_view, name="privacy-politic"),
 ]
--- cpc/app/views.py
+++ cpc/app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, "app/index.html")
+
+
+def privacy_view(request):
+    return render(request, "app/privacy.html")
