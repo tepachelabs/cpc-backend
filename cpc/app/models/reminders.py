a6a998a29f980d1c8041efff7f0bd8e25a7f83f2
Jose Salcido <jmsalcidoaguilar@gmail.com>
2024-05-16 16:02:24 -0700

    create db model

--- cpc/app/models/reminders.py
+++ cpc/app/models/reminders.py
@@ -2,6 +2,21 @@ from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
 
+class CollectionReminder(TimeStampedModel):
+    shopify_id = models.BigIntegerField(
+        blank=False,
+        null=False,
+        help_text="Shopify ID of the collection",
+    )
+
+    slug = models.SlugField(
+        max_length=100,
+        blank=False,
+        null=False,
+        help_text="Slug of the collection for internal tracking only",
+    )
+
+
 class ProductReminder(TimeStampedModel):
     shopify_id = models.BigIntegerField(
         blank=True,
