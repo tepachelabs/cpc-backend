3f11fe54da6ad671aa062a7524c16a338e44c816
Jose Salcido <jmsalcidoaguilar@gmail.com>
2024-05-16 16:03:24 -0700

    add admin model

--- cpc/app/admin.py
+++ cpc/app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import ProductReminder
+from .models import ProductReminder, CollectionReminder
+
+
+@admin.register(CollectionReminder)
+class CollectionReminderAdmin(admin.ModelAdmin):
+    list_display = ("id", "slug", "shopify_id")
 
 
 @admin.register(ProductReminder)
