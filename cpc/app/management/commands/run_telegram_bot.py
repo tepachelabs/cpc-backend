a1c8792402f6bb5f017db5c243e3565852646611
Jose Salcido <jmsalcidoaguilar@gmail.com>
2024-08-01 16:40:24 -0700

    add a better way for the telegram bot to add commands in the future

--- cpc/app/management/commands/run_telegram_bot.py
+++ cpc/app/management/commands/run_telegram_bot.py
@@ -1,43 +1,15 @@
-import asyncio
 import logging
 
-from django.conf import settings
 from django.core.management import BaseCommand
-from telegram import Update, BotCommand
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
 
-from cpc.celery import app as celery_app
+from cpc.app.telegram.bot import TelegramBot
 
 logger = logging.getLogger()
 
 
 class Command(BaseCommand):
     help = "Run Telegram Bot"
-
-    @staticmethod
-    async def inventory_reminder_handler(_update: Update, _context: CallbackContext):
-        # Keep it as a celery task as the cron job is already set up
-        celery_app.send_task("cpc.shopify_backend.tasks.inventory_reminder")
-
-    @staticmethod
-    async def post_init(application):
-        inventario_command = BotCommand("inventario", "Recordatorio de inventario")
-        await application.bot.set_my_commands(commands=[inventario_command])
+    bot = TelegramBot()
 
     def handle(self, *args, **options):
-        if settings.TELEGRAM_BOT_TOKEN is None:
-            logger.error("TELEGRAM_BOT_TOKEN is not set in the environment variables.")
-            return
-
-        start_handler = CommandHandler("inventario", self.inventory_reminder_handler)
-
-        application = (
-            ApplicationBuilder()
-            .token(settings.TELEGRAM_BOT_TOKEN)
-            .post_init(self.post_init)
-            .build()
-        )
-        application.add_handler(start_handler)
-
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(application.run_polling())
+        self.bot.start()
---
+++ cpc/app/telegram/commands.py
@@ -0,0 +1,38 @@
+from telegram import Update
+from telegram.ext import CommandHandler, CallbackContext
+
+from cpc.app.services.chat_messages import CalendarChatMessageService
+from cpc.app.services.telegram import TelegramMessageParser
+from cpc.celery import app as celery_app
+
+
+class BaseCommand:
+    command_name = None
+
+    async def handler(self, _update: Update, _context: CallbackContext):
+        raise NotImplementedError
+
+    def command_handler(self) -> CommandHandler:
+        return CommandHandler(self.command_name, self.handler)
+
+
+class InventarioCommand(BaseCommand):
+    command_name = "inventario"
+    command_description = "Verificar inventario en Shopify POS"
+    task_executor = celery_app
+
+    async def handler(self, _update: Update, _context: CallbackContext):
+        # Keep it as a celery task as the cron job is already set up
+        print("Hehehehehehe")
+        self.task_executor.send_task("cpc.shopify_backend.tasks.inventory_reminder")
+
+
+class EventosCommand(BaseCommand):
+    command_name = "eventos"
+    command_description = "Verificar eventos de hoy de Google Calendar"
+
+    async def handler(self, _update: Update, _context: CallbackContext):
+        calendar_message_service = CalendarChatMessageService(
+            telegram_message_parser=TelegramMessageParser()
+        )
+        calendar_message_service.notify_today_events()
