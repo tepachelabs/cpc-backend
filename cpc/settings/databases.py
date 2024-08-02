9d79a10064f6a75cb632935db201a47a7739ecd4
Jose Salcido <jmsalcidoaguilar@gmail.com>
2024-08-01 17:47:19 -0700

    Remove multiple warnings with pylint (#10)
    * remove multiple warnings with pylint
    
    * pylint remove

--- cpc/settings/databases.py
+++ cpc/settings/databases.py
@@ -4,9 +4,6 @@ import dj_database_url
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-from cpc.settings import BASE_DIR, PRODUCTION
-
-# TODO change this to use the DATABASE_URL
 DATABASES = {"default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))}
 
 # Default primary key field type
--- cpc/shopify_backend/management/commands/inventory_reminder.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-from cpc.shopify_backend.tasks import inventory_reminder
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
-        inventory_reminder()
