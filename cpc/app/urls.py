56c6f9e55a5279f4902a73823d11d7c8e7a56cd3
tonymtz <atony.mtz@gmail.com>
2024-08-20 15:08:27 -0700

    register url for new endpoint

--- cpc/app/urls.py
+++ cpc/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index_view, name="index"),
     path("privacy", views.privacy_view, name="privacy-politic"),
+    path("products", views.ProductsView.as_view(), name="index"),
 ]
