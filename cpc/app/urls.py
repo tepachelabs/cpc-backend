82872fe340461762b65258e512fc9a693e010cea
Jose Salcido <jmsalcidoaguilar@gmail.com>
2024-08-26 20:34:54 -0700

    run poetry black

--- cpc/app/urls.py
+++ cpc/app/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index_view, name="index"),
     path("privacy", views.privacy_view, name="privacy-politic"),
-
     # API
     path("api/products", views.ProductsView.as_view()),
     path("api/products/<str:id>", views.ProductsView.as_view()),
